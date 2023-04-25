cf9a11d8f7bdb06e56f8446eb2fc42855ae4e963
marce1l <mikesmarcell@gmail.com>
2023-04-25 21:06:42 +0200

    Revert "small update for uniformity"
    This reverts commit cf1614c865c91824ef87efe7ec5bd65b15c1b415.

--- 1d_perlin.py
+++ 1d_perlin.py
@@ -1,6 +1,7 @@
 import math
 import time
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 # pseudo-random number generator
 class PseudoRandomGenerator():
@@ -19,12 +20,12 @@ class PseudoRandomGenerator():
         return self.Z / self.M - 0.5
 
 
-class Perlin_1D():
+class PerlinNoise():
     
-    def noise(self, amplitude, wavelength, width, octaves, divisor=2):
-        return self.combine_octaves(self.generate_octave(amplitude, wavelength, width, octaves, divisor))
+    def Noise(self):
+        pass
     
-    def combine_octaves(self, octave):
+    def CombineOctaves(self, octave):
         combinedOctaves = []
         
         for i in range(0, len(octave[0])):
@@ -35,17 +36,17 @@ class Perlin_1D():
         return combinedOctaves
     
     # octave generator      rename ?
-    def generate_octave(self, amplitude, wavelength, width, octaves, divisor):
+    def generateOctave(self, amplitude, wavelength, width, octaves, divisor=2):
         octave = []
         
         for i in range(0, octaves):
-            octave.append(self.perlin(amplitude, wavelength, width))
+            octave.append(self.Perlin(amplitude, wavelength, width))
             amplitude = amplitude / divisor
             wavelength = wavelength / divisor
         return octave
     
     # 1D Perlin line generator
-    def perlin(self, amplitude, wavelength, width):
+    def Perlin(self, amplitude, wavelength, width):
         prg = PseudoRandomGenerator(time.time())
         a = prg.generate()
         b = prg.generate()
@@ -58,23 +59,42 @@ class Perlin_1D():
                 b = prg.generate()
                 pos.append(a * amplitude)
             else:
-                pos.append(self.interpolate(a, b, (x % wavelength) / wavelength) * amplitude)
+                pos.append(self.Interpolate(a, b, (x % wavelength) / wavelength) * amplitude)
             x += 1
         return pos
 
     # cosine interpolation
-    def interpolate(self, y1, y2, x):
+    def Interpolate(self, y1, y2, x):
         xv = (1 - math.cos(x * math.pi)) * 0.5
         return y1 + (y2 - y1) * xv
 
-    def plot_noise(self, noise):
-        Time = [i for i in range(0, len(noise))]
-        plt.plot(Time, noise)
-        
-        plt.show()
+
+def plot_noise(noise):
+    Time = [i for i in range(0, len(noise[0]))]
+    
+    bins = 30
+    
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(9, 6))
+    fig.suptitle('Distribution')
+    n, x, _ = ax1.hist(noise[0], bins=bins, density=True, color='blue', ec='black')
+    ax1.plot(x, stats.gaussian_kde(noise[0])(x), color='black', lw=2.5)
+    n, x, _ = ax2.hist(noise[1], bins=bins, density=True, color='orange', ec='black')
+    ax2.plot(x, stats.gaussian_kde(noise[1])(x), color='black', lw=2.5)
+    n, x, _ = ax3.hist(noise[2], bins=bins, density=True, color='green', ec='black')
+    ax3.plot(x, stats.gaussian_kde(noise[2])(x), color='black', lw=2.5)
+    n, x, _ = ax4.hist(noise[3], bins=bins, density=True, color='red', ec='black')
+    ax4.plot(x, stats.gaussian_kde(noise[3])(x), color='black', lw=2.5)
 
 
-# not integrated yet
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(13, 6))
+    fig.suptitle('Perlin noise')
+    ax1.plot(Time, noise[0], color='blue')
+    ax2.plot(Time, noise[1], color='orange')
+    ax3.plot(Time, noise[2], color='green')
+    ax4.plot(Time, noise[3], color='red')
+    
+    plt.show()
+
 def rate_limit_value(signal, previous_output, limiter, Ts):
     if abs((signal - previous_output) / Ts) > limiter:
         exp_sign = np.sign(signal - previous_output)
@@ -84,11 +104,10 @@ def rate_limit_value(signal, previous_output, limiter, Ts):
     return value
 
 def main():
-    perlin_1D = Perlin_1D()
+    pn = PerlinNoise()
 
-    noise = perlin_1D.noise(amplitude=1.0, wavelength=512.0, width=10000, octaves=12)
-    perlin_1D.plot_noise(noise)
-
+    noise = [pn.CombineOctaves(pn.generateOctave(amplitude=1, wavelength=512, width=10000, octaves=12)) for i in range(4)]
+    plot_noise(noise)
 
 if __name__ == "__main__":
     main()
--- 2d_perlin.py
+++ 2d_perlin.py
@@ -1,170 +1,70 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-import time
-import datetime
 
+np.random.seed(6187254)
 
-class Perlin_2D():
-    # noise = [[]]
+gradX = [[np.random.rand() for i in range(256)] for i in range(256)]
+gradY = [[np.random.rand() for i in range(256)] for i in range(256)]
+
+step  = 0.01
+width = 1000
+
+def toUnitVector():
+    for i in range(256):
+        for j in range(256):
+            c = math.sqrt(pow(gradX[i][j], 2) + pow(gradY[i][j], 2))
+            gradX[i][j] = gradX[i][j] / c
+            gradY[i][j] = gradY[i][j] / c
+
+def perlin(x, y):
+    xi = int(x)
+    yi = int(y)
     
-    # gradient vectors
-    gradX = [[]]
-    gradY = [[]]
-
-
-    def perlin(self, x, y):
-        # grid top left index
-        xi = int(x)
-        yi = int(y)
-        
-        # top left distance vector
-        xd = x - xi
-        yd = y - yi
-        
-        # top left, top right, bottom left, bottom right dot product
-        p00 = xd     * self.gradX[yi][xi]     + yd     * self.gradY[yi][xi]
-        p01 = (xd-1) * self.gradX[yi][xi+1]   + yd     * self.gradY[yi][xi+1]
-        p10 = xd     * self.gradX[yi+1][xi]   + (yd-1) * self.gradY[yi+1][xi]
-        p11 = (xd-1) * self.gradX[yi+1][xi+1] + (yd-1) * self.gradY[yi+1][xi+1]
-
-        # fade for smooth linear interpolation
-        xf = self.fade(xd)
-        yf = self.fade(yd)
-        
-        a = self.interpolate(p00, p10, yf)
-        b = self.interpolate(p01, p11, yf)
-        return self.interpolate(a, b, xf)
-
-    def fade(self, f):
-        return 6 * f**5 - 15 * f**4 + 10 * f**3
-
-    # linear interpolation
-    def interpolate(self, a, b, x):
-        return a + x * (b - a)
-
-    def create_gradientVectors(self, seed):
-        self.set_seed(seed)
-
-        gradX = np.random.rand(256,256) * 2 - 1
-        gradY = np.random.rand(256,256) * 2 - 1
-
-        # convert to unit vectors
-        c = np.sqrt(gradX ** 2 + gradY ** 2)
-        gradX = gradX / c
-        gradY = gradY / c
-        
-        return (gradX, gradY)
-
-    def set_seed(self, seed):
-        # -1000000000 to make it work for a century (2**32 numpy limit)
-        offset = -1000000000
-        
-        # Note: python 2 and python 3 time.time() differs in precision
-        if seed is None:
-            seed = int(time.time())+offset
-            
-        try:
-            np.random.seed(seed)
-        except TypeError as e:
-            seed = int(time.time())+offset
-            print("TypeError: %s" % e)
-            print("Current time is used as seed")
-        except ValueError as e:
-            seed = int(time.time())+offset
-            print("ValueError: %s" % e)
-            print("Current time is used as seed")
-        
-        np.random.seed(seed)
-        # self.store_seed(seed, offset)
+    # print("\nx: %s y: %s" % (x,y))
+    # print("xi: %s yi: %s" % (xi,yi))
     
-    def store_seed(self, seed, offset):
-        seed = seed+(-offset)
-        print(datetime.datetime.fromtimestamp(seed))
-        raise NotImplementedError
+    xd = x - xi
+    yd = y - yi
     
-    def noise(self, amplitude, width, step, seed=None):
-        self.gradX, self.gradY = self.create_gradientVectors(seed)
-        noise = np.empty((width, width))
+    # print("xd: %s yd: %s" % (xd,yd))
     
-        for y in range(width):
-            for x in range(width):
-                noise[y][x] = self.perlin(x*step, y*step)*amplitude*2       # *2 because not sure if normal range is [-0.5, 0.5] or [-1, 1]
-        return noise
+    p00 = xd     * gradX[xi][yi]     + yd     * gradY[xi][yi]
+    p01 = (xd-1) * gradX[xi+1][yi]   + yd     * gradY[xi+1][yi]
+    p10 = xd     * gradX[xi][yi+1]   + (yd-1) * gradY[xi][yi+1]
+    p11 = (xd-1) * gradX[xi+1][yi+1] + (yd-1) * gradY[xi+1][yi+1]
 
-    def generate_octave(self):
-        raise NotImplementedError
+    xf = fade(xd)
+    yf = fade(yd)
+    
+    x1 = interpolation(p00, p10, yf)
+    x2 = interpolation(p01, p11, yf)
+    
+    # print("p00: %s p01: %s p10: %s p11: %s" % (p00, p01, p10, p11))
+    
+    return interpolation(x1, x2, xf)
 
-    def combine_octaves(self):
-        raise NotImplementedError
+def interpolation(a, b, x):
+    return a + x * (b - a)
+    
+def fade(f):
+    return 6 * f**5 - 15 * f**4 + 10 * f**3
 
-    def plot_noise(self, noise):
-        Time = [i for i in range(len(noise))]
-        
-        plt.plot(Time, noise[0])
-        plt.show()
-        
-        plt.imshow(noise, cmap='gray')
-        plt.show()
+def makeNoise():
+    toUnitVector()
 
-
-def generate_noise(amplitude, step, width):
+    noise = np.empty((width, width))
+    
     for y in range(width):
         for x in range(width):
             noise[y][x] = perlin(x*step, y*step)
-    return noise
     
-def generateOctave(octaves, amplitude, width, step, divisor=2):
-    octave = []
+    Time = [i for i in range(width)]
     
-    for i in range(octaves):
-        octave.append(generate_noise(amplitude, step, width))
-        amplitude = amplitude / divisor
-        step      = step      / divisor
-    return octave
-
-def combineOctave(octave):
-    combinedNoise = np.empty(len(octave[0][0], len(octave[0][0])))
+    plt.plot(Time, noise[0])
+    plt.show()
     
-    for y in range(len(octave[0])):
-        for x in range (len(octave[0][0])):            
-            total = 0
-            for i in range(len(octave)):
-                total += octave[k][y][x]
-            combinedNoise[y][x] = total
-    return combinedNoise
+    plt.imshow(noise, cmap='gray')
+    plt.show()
 
-
-def main():
-    perlin_2D = Perlin_2D()
-    perlin_2D.plot_noise(perlin_2D.noise(amplitude=1, width=1000, step=0.01, seed=142))
-
-
-if __name__ == "__main__":
-    main()
-
-
-######################################
-#           RATE LIMITER             #
-######################################
-
-def rate_limit_value(signal, previous_output, limiter, Ts):
-    if abs((signal - previous_output) / Ts) > limiter:
-        exp_sign = np.sign(signal - previous_output)
-        value = previous_output + exp_sign * limiter * Ts
-    else:
-        value = signal
-    return value
-
-def test_step(self):
-        t = np.arange(0,1.5,0.001)
-        signal = np.append(3 * np.ones(500), -3*np.ones(500))
-        signal = np.pad(signal, (0, len(t)- len(signal)), "constant")
-        ratelimited = [signal[0]]
-        
-        for i in range(1,len(signal)):
-            ratelimited.append(rate_limit_value(signal[i], ratelimited[-1], 10, 0.01))
-            
-        plt.plot(t, signal, label = "Wheel angle [deg]", color = REFERENCE_COLOR, alpha = 0.8)
-        plt.plot(t, ratelimited, label = "Rate limited wheel angle [deg]", color = "firebrick")
-        plot_settings()
-        plt.show()
+makeNoise()
