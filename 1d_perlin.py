cf1614c865c91824ef87efe7ec5bd65b15c1b415
marce1l <mikesmarcell@gmail.com>
2023-04-25 21:01:28 +0200

    small update for uniformity
    removed plot that prevented python 2 use
    now uses python function naming conventions

--- 1d_perlin.py
+++ 1d_perlin.py
@@ -1,7 +1,6 @@
 import math
 import time
 import matplotlib.pyplot as plt
-import scipy.stats as stats
 
 # pseudo-random number generator
 class PseudoRandomGenerator():
@@ -20,12 +19,12 @@ class PseudoRandomGenerator():
         return self.Z / self.M - 0.5
 
 
-class PerlinNoise():
+class Perlin_1D():
     
-    def Noise(self):
-        pass
+    def noise(self, amplitude, wavelength, width, octaves, divisor=2):
+        return self.combine_octaves(self.generate_octave(amplitude, wavelength, width, octaves, divisor))
     
-    def CombineOctaves(self, octave):
+    def combine_octaves(self, octave):
         combinedOctaves = []
         
         for i in range(0, len(octave[0])):
@@ -36,17 +35,17 @@ class PerlinNoise():
         return combinedOctaves
     
     # octave generator      rename ?
-    def generateOctave(self, amplitude, wavelength, width, octaves, divisor=2):
+    def generate_octave(self, amplitude, wavelength, width, octaves, divisor):
         octave = []
         
         for i in range(0, octaves):
-            octave.append(self.Perlin(amplitude, wavelength, width))
+            octave.append(self.perlin(amplitude, wavelength, width))
             amplitude = amplitude / divisor
             wavelength = wavelength / divisor
         return octave
     
     # 1D Perlin line generator
-    def Perlin(self, amplitude, wavelength, width):
+    def perlin(self, amplitude, wavelength, width):
         prg = PseudoRandomGenerator(time.time())
         a = prg.generate()
         b = prg.generate()
@@ -59,42 +58,23 @@ class PerlinNoise():
                 b = prg.generate()
                 pos.append(a * amplitude)
             else:
-                pos.append(self.Interpolate(a, b, (x % wavelength) / wavelength) * amplitude)
+                pos.append(self.interpolate(a, b, (x % wavelength) / wavelength) * amplitude)
             x += 1
         return pos
 
     # cosine interpolation
-    def Interpolate(self, y1, y2, x):
+    def interpolate(self, y1, y2, x):
         xv = (1 - math.cos(x * math.pi)) * 0.5
         return y1 + (y2 - y1) * xv
 
-
-def plot_noise(noise):
-    Time = [i for i in range(0, len(noise[0]))]
-    
-    bins = 30
-    
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(9, 6))
-    fig.suptitle('Distribution')
-    n, x, _ = ax1.hist(noise[0], bins=bins, density=True, color='blue', ec='black')
-    ax1.plot(x, stats.gaussian_kde(noise[0])(x), color='black', lw=2.5)
-    n, x, _ = ax2.hist(noise[1], bins=bins, density=True, color='orange', ec='black')
-    ax2.plot(x, stats.gaussian_kde(noise[1])(x), color='black', lw=2.5)
-    n, x, _ = ax3.hist(noise[2], bins=bins, density=True, color='green', ec='black')
-    ax3.plot(x, stats.gaussian_kde(noise[2])(x), color='black', lw=2.5)
-    n, x, _ = ax4.hist(noise[3], bins=bins, density=True, color='red', ec='black')
-    ax4.plot(x, stats.gaussian_kde(noise[3])(x), color='black', lw=2.5)
+    def plot_noise(self, noise):
+        Time = [i for i in range(0, len(noise))]
+        plt.plot(Time, noise)
+        
+        plt.show()
 
 
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(13, 6))
-    fig.suptitle('Perlin noise')
-    ax1.plot(Time, noise[0], color='blue')
-    ax2.plot(Time, noise[1], color='orange')
-    ax3.plot(Time, noise[2], color='green')
-    ax4.plot(Time, noise[3], color='red')
-    
-    plt.show()
-
+# not integrated yet
 def rate_limit_value(signal, previous_output, limiter, Ts):
     if abs((signal - previous_output) / Ts) > limiter:
         exp_sign = np.sign(signal - previous_output)
@@ -104,10 +84,11 @@ def rate_limit_value(signal, previous_output, limiter, Ts):
     return value
 
 def main():
-    pn = PerlinNoise()
+    perlin_1D = Perlin_1D()
 
-    noise = [pn.CombineOctaves(pn.generateOctave(amplitude=1, wavelength=512, width=10000, octaves=12)) for i in range(4)]
-    plot_noise(noise)
+    noise = perlin_1D.noise(amplitude=1.0, wavelength=512.0, width=10000, octaves=12)
+    perlin_1D.plot_noise(noise)
+
 
 if __name__ == "__main__":
     main()
